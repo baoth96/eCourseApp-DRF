c37a48ec5fab648139819e640fd5d814f40baed6
baoth96 <thaihoangbao112@gmail.com>
2022-12-29 11:36:33 +0700

    api-drf

---
+++ courses/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='uploads/%Y/%m')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, unique=True)
+
+    def __str__(self):
+        return self.name
---
+++ courses/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets, generics
+from .models import Category
+from .serializers import CategorySerializer
+
+class CategoryViewSet(viewsets.ViewSet, generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
